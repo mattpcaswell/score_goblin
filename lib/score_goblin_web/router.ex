50827109cf5aacadd8a78abaae0f7817fe5ee04a
Matt Caswell <mattpcaswell@gmail.com>
2023-10-23 23:34:52 -0400

    Autogen accounts w/ login

--- lib/score_goblin_web/router.ex
+++ lib/score_goblin_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ScoreGoblinWeb.Router do
   use ScoreGoblinWeb, :router
 
+  import ScoreGoblinWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule ScoreGoblinWeb.Router do
     plug :put_root_layout, html: {ScoreGoblinWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -42,6 +45,44 @@ defmodule ScoreGoblinWeb.Router do
 
       live_dashboard "/dashboard", metrics: ScoreGoblinWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", ScoreGoblinWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ScoreGoblinWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", ScoreGoblinWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ScoreGoblinWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", ScoreGoblinWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{ScoreGoblinWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
